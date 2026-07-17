73dc743cf1ad1088f671e24af49dae0d822b5e9b
Anders Olson <sredna43@gmail.com>
2026-07-17 15:06:15 -0500

    move to vanilla solid app

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,17 @@
-import { Router } from "@solidjs/router";
+import { Route, Router } from "@solidjs/router";
 import "./app.css";
 import { Suspense } from "solid-js";
-import { FileRoutes } from "@solidjs/start/router";
 import { MetaProvider, Meta } from "@solidjs/meta";
+import Home from "./components/home";
+import Game from "./components/game";
 
 export default function App() {
   return (
     <MetaProvider>
       <Meta name="viewport" content=" initial-scale=1.0, user-scalable=no" />
       <Router root={(props) => <Suspense>{props.children}</Suspense>}>
-        <FileRoutes />
+        <Route path="/" component={Home} />
+        <Route path="/game" component={Game} />
       </Router>
     </MetaProvider>
   );
