a64b363264c324c852b87815f735b4194e04dd03
Anders Olson <sredna43@gmail.com>
2026-07-17 14:28:20 -0500

    cli setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
-import vercel from "solid-start-vercel";
 
 import { solidStart } from "@solidjs/start/config";
 
 export default defineConfig({
   plugins: [
-    solidStart({ adapter: vercel({ edge: true }), ssr: false }),
+    solidStart({ ssr: false }),
   ]
 });
