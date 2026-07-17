0ba4c407e3e28177101f70c48e03fe0fac57496f
Anders Olson <sredna43@gmail.com>
2026-07-17 14:33:28 -0500

    try anything

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "vite";
 import { solidStart } from "@solidjs/start/config";
 
 export default defineConfig({
+  server: {
+    preset: "vercel"
+  },
   plugins: [
     solidStart({ ssr: false }),
   ]
